0f2c466efaabd390eccb0dda8b7697761b91e416
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-08-11 17:37:38 +0800

    add getChangesFrom

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Patches PSGC data with Summary of Changes excel file from PSA
 ## TODO:
 
 -   [x] Divide by release date
--   [ ] Get range of changes [start release and end release (defaults to latest)]
+-   [x] Get range of changes [start release and end release (defaults to latest)]
 -   [ ] Handle each change type
 -   [ ] Callbacks for changes
--- src/reader.ts
+++ src/reader.ts
@@ -71,32 +71,91 @@ class Reader {
             previousRow = row;
         }
 
+        return this;
+    }
+
+    /**
+     * Get the changes with the given range (inclusive)
+     * @param from Title to start from
+     * @param to Last title to include (defaults to latest)
+     * @returns Changes from start to end
+     */
+    getChangesFrom(from: string, to?: string) {
+        const releases = Object.keys(this.changes);
+        const changes: Row[] = [];
+
+        let isInRange = false;
+        for (const release of releases) {
+            if (release === from) {
+                isInRange = true;
+            }
+
+            if (isInRange) {
+                changes.push(...this.changes[release]);
+            }
+
+            if (release === to) {
+                break;
+            }
+        }
+
         return changes;
     }
 }
 
 const reader = new Reader();
-const changes = reader.read("./data/PSGC-2Q-2024-Summary-of-Changes.xlsx");
 
-changes.then((changes) => {
+reader.read("./data/PSGC-2Q-2024-Summary-of-Changes.xlsx").then((reader) => {
     console.log(
-        "October - December 2004 Updates",
-        changes["October - December 2004 Updates"].map((row) => row[Index.name])
+        reader
+            .getChangesFrom(
+                "October - December 2004 Updates",
+                "July - September 2005 Updates"
+            )
+            .map((row) => row[Index.name])
     );
     console.log(
-        "July - September 2005 Updates",
-        changes["July - September 2005 Updates"].map((row) => row[Index.name])
+        reader
+            .getChangesFrom(
+                "July - September 2019 Updates",
+                "January - March 2020"
+            )
+            .map((row) => row[Index.name])
     );
     console.log(
-        "January - March 2006 Updates",
-        changes["January - March 2006 Updates"].map((row) => row[Index.name])
-    );
-    console.log(
-        "July - September 2006 Updates",
-        changes["July - September 2006 Updates"].map((row) => row[Index.name])
-    );
-    console.log(
-        "January to March 2024",
-        changes["January to March 2024"].map((row) => row[Index.name])
+        reader.getChangesFrom("April 2024").map((row) => row[Index.name])
     );
 });
+
+// reader
+//     .read("./data/PSGC-2Q-2024-Summary-of-Changes.xlsx")
+//     .then(({ changes }) => {
+//         console.log(
+//             "October - December 2004 Updates",
+//             changes["October - December 2004 Updates"].map(
+//                 (row) => row[Index.name]
+//             )
+//         );
+//         console.log(
+//             "July - September 2005 Updates",
+//             changes["July - September 2005 Updates"].map(
+//                 (row) => row[Index.name]
+//             )
+//         );
+//         console.log(
+//             "January - March 2006 Updates",
+//             changes["January - March 2006 Updates"].map(
+//                 (row) => row[Index.name]
+//             )
+//         );
+//         console.log(
+//             "July - September 2006 Updates",
+//             changes["July - September 2006 Updates"].map(
+//                 (row) => row[Index.name]
+//             )
+//         );
+//         console.log(
+//             "January to March 2024",
+//             changes["January to March 2024"].map((row) => row[Index.name])
+//         );
+//     });

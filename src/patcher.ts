3237b205e3d03479cc392223775973858d30b0cd
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-08-15 20:23:01 +0800

    rename folder to handlers

--- src/patcher.ts
+++ src/patcher.ts
@@ -1,5 +1,5 @@
 import * as actions from "./actions";
-import { Callback } from "./callbacks/callback";
+import { Callback } from "./handlers/callback";
 import { Entry } from "./types";
 import { getGeoLevel } from "./utils";
 

8c3e9312e5fe92067009d2d381722f6c6d798d90
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-08-16 21:29:08 +0800

    definitions for each geo level

--- src/handlers/definition.ts
+++ src/handlers/definition.ts
@@ -1,4 +1,40 @@
 export interface Definition {
+    tableName: string;
+    id?: string;
+    oldCode?: string;
     code: string;
     name: string;
+}
+
+/**
+ * Region
+ */
+export type Region = Definition;
+
+/**
+ * Province
+ * - can reference region
+ */
+export interface Province extends Region {
+    regionId?: string;
+}
+
+/**
+ * City
+ * - can reference province
+ */
+export interface City extends Province {
+    provinceId?: string;
+}
+
+export type Municipality = City;
+
+export interface SubMunicipality extends Definition {
+    cityId?: string;
+}
+
+export interface Barangay extends Definition {
+    cityId?: string;
+    municipalityId?: string;
+    subMunicipalityId?: string;
 }
